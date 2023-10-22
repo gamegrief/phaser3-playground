fd36e146a8943d50be3775c441a2c8b5c47f7c5b
gamedrift <gamedrift451@gmail.com>
2023-10-22 16:55:59 +0800

    added public assets

---
+++ src/scenes/Game.ts
@@ -0,0 +1,17 @@
+import Phaser from 'phaser'
+
+export default class Game extends Phaser.Scene
+{
+	constructor()
+	{
+		super('game')
+	}
+
+	preload()
+    {
+    }
+
+    create()
+    {
+    }
+}
