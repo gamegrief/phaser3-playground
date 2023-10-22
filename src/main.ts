d0bc074a04184550e7e4d795ca4939dd7a7a5c8a
gamedrift <gamedrift451@gmail.com>
2023-10-22 18:20:19 +0800

    completed part 1 of tutorial with map rendering

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,32 @@
 import Phaser from 'phaser'
-
+import Preloader from './scenes/Peloader'
 import Game from './scenes/Game'
 
-const config: Phaser.Types.Core.GameConfig = {
+// const config: Phaser.Types.Core.GameConfig = {
+// 	type: Phaser.AUTO,
+// 	width: 400,
+// 	height: 250,
+// 	physics: {
+// 		default: 'arcade',
+// 		arcade: {
+// 			gravity: { y: 0 }
+// 		}
+// 	},
+// 	scene: [Preloader, Game]
+// }
+
+export default new Phaser.Game({
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	width: 400,
+	height: 250,
 	physics: {
 		default: 'arcade',
 		arcade: {
 			gravity: { y: 0 }
 		}
 	},
-	scene: [Game]
-}
-
-export default new Phaser.Game(config)
+	scene: [Preloader, Game],
+	scale:{
+		zoom: 2
+	}
+})
